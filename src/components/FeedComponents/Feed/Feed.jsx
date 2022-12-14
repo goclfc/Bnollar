60a2aeb897b56db1569455e56a9ac81d6849e03a
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2022-12-14 15:04:11 +0400

    active buttons handling
    Updates active button

--- src/components/FeedComponents/Feed/Feed.jsx
+++ src/components/FeedComponents/Feed/Feed.jsx
@@ -1,39 +1,35 @@
-import React, {useState} from 'react'
-import SwipeProfiles from '../Swipe_Profiles/SwipeProfiles'
-import "./Feed.css"
-import SortBtn from './SortBtn'
+import React, { useState,useEffect } from "react";
+import SwipeProfiles from "../Swipe_Profiles/SwipeProfiles";
+import "./Feed.css";
+import SortBtn from "./SortBtn";
 const Feed = () => {
   
-     
-
-       
-
-      
-   
-      
-    
-     const [activeState,setActiveState] = useState('New')
-
-
-
-      const active =()=> {
-        setActiveState('Favourite')
-        console.log(activeState)
-      }
-           
+  const [activeState, setActiveState] = useState("New");
   return (
-    <div className="feed" >
-     
-        <SwipeProfiles  />
-        
-        <div className="sort_btns_wrapper"  > 
-       <SortBtn  className='sort-btn' setActiveState={setActiveState} activeState={activeState}  parent="New"/>
-       <SortBtn className='sort-btn' setActiveState={setActiveState} activeState={activeState}  parent="Popular"/>   
-       <SortBtn className='sort-btn' setActiveState={setActiveState} activeState={activeState}  parent="Favourite"/>
-      
-        </div>
+    <div className="feed">
+      <SwipeProfiles />
+      <div className="sort_btns_wrapper">
+        <SortBtn
+          className="sort-btn"
+          setActiveState={setActiveState}
+          activeState={activeState}
+          parent="New"
+        />
+        <SortBtn
+          className="sort-btn"
+          setActiveState={setActiveState}
+          activeState={activeState}
+          parent="Popular"
+        />
+        <SortBtn
+          className="sort-btn"
+          setActiveState={setActiveState}
+          activeState={activeState}
+          parent="Favourite"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/components/FeedComponents/Feed/SortBtn.jsx
+++ src/components/FeedComponents/Feed/SortBtn.jsx
@@ -1,25 +1,38 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
+import { act } from "react-dom/test-utils";
 
-const SortBtn = ({parent, setActiveState, activeState}) => {
-    const [active, setActive] = useState(parent === activeState)
-    const [hoverBorder, setHoverBorder] = useState(false)
-   
-    const changeActive = () => {
-  
-        setActiveState(parent)
+const SortBtn = ({ parent, setActiveState, activeState }) => {
+  const [active, setActive] = useState(parent === activeState);
+  const [hoverBorder, setHoverBorder] = useState(false);
+  const changeActive = () => {
+    setActiveState(parent);
+    setHoverBorder(!hoverBorder);
+  };
+  useEffect(() => {
+    if (parent === activeState) {
+      setActive(true);
+    } else {
+      setActive(false);
     }
-
-    const activeHandler = () => {
-      setActive(!active)
-    }
+  }, [activeState, active]);
   return (
     <>
-    {active ?
-        <button id={ 'activeSort'} onClick={()=> setActiveState(!active)}>{parent}</button> :
-        <button id={hoverBorder && 'activeSort'} onMouseOver={()=> setHoverBorder(true)} onMouseLeave={()=> setHoverBorder(false)} onClick={() => changeActive()}>{parent}</button>
-    }
+      {active ? (
+        <button id={"activeSort"} onClick={() => setActive(!active)}>
+          {parent}
+        </button>
+      ) : (
+        <button
+          id={hoverBorder && "activeSort"}
+          onMouseOver={() => setHoverBorder(true)}
+          onMouseLeave={() => setHoverBorder(false)}
+          onClick={() => changeActive()}
+        >
+          {parent}
+        </button>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default SortBtn
+export default SortBtn;
